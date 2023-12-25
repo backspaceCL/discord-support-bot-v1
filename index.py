4015406c108ca1a6a7a1eeef8296f6172bf30d35
CLoud <84443096+backspaceCL@users.noreply.github.com>
2023-12-25 20:01:06 +0900

    index.py

--- index.py
+++ index.py
@@ -3,18 +3,9 @@ token = "token" #봇 토큰 설정하기
 client = discord.Client = discord.Client(intents=discord.Intents.all())
 
 @client.event
-async def on_ready(): #봇이 준비되었을 때
+async def on_ready(): #봇이 준비되었을 때 뜨는 메시지입니다.
     print("봇 준비 완료!")
     print(client.user)
     print("===========================")
-
-@client.event
-async def on_message(message): #사용자가 메시지를 입력했을 때
-    if message.content == "! 컴퓨터 견적": #만일 사용자가 "! 컴퓨터 견적" 라고 입력했을 때
-        await message.channel.send("! NEX에게 개인 DM 해주세요.") #봇이 "! NEX에게 개인 DM 해주세요." 라고 답한다.
-    if message.content == "! 컴퓨터 문제점": #만일 사용자가 "! 컴퓨터 문제점" 라고 입력했을 때
-        await message.channel.send("! NEX에게 어떤 종류의 문제이고, 먼저 충분히 검색 해보시고 개인 DM 해주세요.") #봇이 "! NEX에게 어떤 종류의 문제이고, 먼저 충분히 검색 해보시고 개인 DM 해주세요." 라고 답한다.
-    if message.content == "! 커스텀 키보드 견적": #만일 사용자가 "! 커스텀 키보드 견적" 라고 입력했을 때
-        await message.channel.send("! NEX에게 선호하시는 축, 사용 용도 등 작성해서 개인 DM 해주세요.") #봇이 "! NEX에게 선호하시는 축, 사용 용도 등 작성해서 개인 DM 해주세요." 라고 답한다.
 
 client.run(token)
